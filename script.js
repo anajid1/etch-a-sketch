233bb497ce54521a9d039bc1a69525c8252d5529
Abdullah Najid <abdullah.najid16@gmail.com>
2022-12-23 22:44:46 -0500

    Create 16x16 grid of boxes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+const grid = document.querySelector('.grid');
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+
+  for (let j = 0; j < 16; j++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    row.appendChild(box);
+  }
+
+  grid.appendChild(row);
+}
