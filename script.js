4967f1fc49e1b0bdad34e9480828230adc2a5cce
Abdullah Najid <abdullah.najid16@gmail.com>
2022-12-23 23:34:39 -0500

    On mouse hover change box color to black

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ console.log("Hello World");
 
 const grid = document.querySelector('.grid');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 100; i++) {
   const row = document.createElement('div');
   row.classList.add('row');
 
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < 100; j++) {
     const box = document.createElement('div');
     box.classList.add('box');
+
+    box.addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'black';
+    });
+
     row.appendChild(box);
   }
 
